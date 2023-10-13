f4a03571e9b9fc9fff714de10e7b90a644cedc9c
黃文輝 <antory040512@gmail.com>
2023-10-13 10:34:39 +0800

    練習2-2 route改成選擇性

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('r2', function () {
 Route::get('hello/{name}',function($name){
 return'Hello,'.$name;
 });
+Route::get('hello/{name?}',function($name='Everybody'){
+return'Hello,'.$name;
+});
