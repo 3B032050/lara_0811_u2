7430b98891162fd56fe43bb12f2508a27e853489
黃文輝 <antory040512@gmail.com>
2023-10-18 09:45:13 +0800

    修正HomeControlers重複問題

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-public function index()
-{
-return 'index';
-}
-}
-class HomeController extends Controller
-{
     public function index()
     {
         return view('welcome');
